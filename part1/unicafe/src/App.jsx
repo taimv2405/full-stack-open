a0263835a61671234ac999619714784461721f09
taimv2405 <taimv2405@gmail.com>
2026-05-12 08:11:37 +0000

    [1.6] feat(unicafe): setup initial application boilerplate

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react';
+
 const App = () => {
-  return (
-    <div>
-      <p>Hello world</p>
-    </div>
-  );
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return <div>code here</div>;
 };
 
 export default App;
