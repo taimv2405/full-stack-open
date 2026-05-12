ca91e6d342ec39cb238518d4a7954faf9c8c55b1
taimv2405 <taimv2405@gmail.com>
2026-05-12 08:40:08 +0000

    [1.7] feat(unicafe): add statistics for total, average and positive feedback

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,9 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
+  const average = all > 0 ? (good - bad) / all : 0;
+  const positive = all > 0 ? (good / all) * 100 : 0;
 
   return (
     <>
@@ -17,6 +20,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {positive} %</p>
     </>
   );
 };
