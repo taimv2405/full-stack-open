232d1d26600d84f98b61cbb41a717295e320149a
taimv2405 <taimv2405@gmail.com>
2026-05-12 08:47:42 +0000

    [1.8] refactor(unicafe): extract statistics into a separate component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,21 +1,12 @@
 import { useState } from 'react';
 
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad;
   const average = all > 0 ? (good - bad) / all : 0;
   const positive = all > 0 ? (good / all) * 100 : 0;
 
   return (
     <>
-      <h2>give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
-
       <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
@@ -23,6 +14,23 @@ const App = () => {
       <p>all {all}</p>
       <p>average {average}</p>
       <p>positive {positive} %</p>
+    </>
+  );
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h2>give feedback</h2>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   );
 };
