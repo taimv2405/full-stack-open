da3dc6543213da14c57fffa08c33295d9fe545a9
taimv2405 <taimv2405@gmail.com>
2026-05-13 08:28:06 +0000

    [2.6] feat(phonebook): implement logic to add new persons

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,22 +1,38 @@
 import { useState } from 'react';
+import Person from './components/Person';
 
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const handleNameChange = (e) => setNewName(e.target.value);
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    if (!newName.trim()) return;
+
+    const personObject = {
+      name: newName,
+    };
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <Person key={person.name} name={person.name} />
+      ))}
     </div>
   );
 };
