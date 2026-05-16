4aed264eb0c1e71c1d802946a31f47bc4417dae0
taimv2405 <taimv2405@gmail.com>
2026-05-16 11:18:56 +0000

    [2.20] feat(countries): add capital weather data

--- part2/countries/src/services/countryService.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/all';
-
-export const getAll = () =>
-  axios.get(baseUrl).then((response) => response.data);
