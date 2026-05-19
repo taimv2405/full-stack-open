52eedf3677acd497a57c54c49d11dd85c8588551
taimv2405 <taimv2405@gmail.com>
2026-05-19 15:12:11 +0000

    [3.4] feat(phonebook): implement DELETE /api/persons/:id endpoint

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -53,6 +53,14 @@ app.get('/api/persons/:id', (request, response) => {
   response.json(targetPerson);
 });
 
+app.delete('/api/persons/:id', (request, response) => {
+  const { id } = request.params;
+
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
 const PORT = 3001;
 
 app.listen(PORT, () => {
