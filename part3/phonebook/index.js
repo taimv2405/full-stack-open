910600219c7cadaa4fb39b63afe3bc4a24edab79
taimv2405 <taimv2405@gmail.com>
2026-05-20 12:15:08 +0000

    [3.8] feat(phonebook): log POST request body using morgan

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,7 +4,14 @@ var morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(morgan('combined'));
+
+morgan.token('data', (request, response) => {
+  return JSON.stringify(request.body);
+});
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :data'),
+);
 
 const PORT = 3001;
 const MAX_ID_VALUE = 1000000;
