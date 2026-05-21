5184ddeda9b09542bdf7eb6acac62f87a6c88af7
taimv2405 <taimv2405@gmail.com>
2026-05-21 03:26:39 +0000

    [3.9] feat(phonebook): connect frontend to backend via relative url

--- part3/phonebook/frontend/src/services/personService.js
+++ part3/phonebook/frontend/src/services/personService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:3001/persons';
+const baseUrl = '/api/persons';
 
 export const getAllPersons = () =>
   axios.get(baseUrl).then((response) => response.data);
