2e0246e04e5e9d372e8a5b8ccb7a3c0159e024ad
taimv2405 <taimv2405@gmail.com>
2026-05-23 10:40:36 +0000

    [4.5] feat(bloglist): implement favoriteBlog to identify top-liked blog

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,7 +3,16 @@ const dummy = (blogs) => 1;
 const totalLikes = (blogs = []) =>
   blogs.reduce((sum, blog) => sum + blog.likes, 0);
 
+const favoriteBlog = (blogs = []) => {
+  if (blogs.length === 0) return null;
+
+  return blogs.reduce((mostLiked, current) =>
+    current.likes > mostLiked.likes ? current : mostLiked,
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
