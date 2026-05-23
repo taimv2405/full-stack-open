a23424a5fe3c49f78f95c7723bbe7c97a7d6a399
taimv2405 <taimv2405@gmail.com>
2026-05-23 07:55:36 +0000

    [4.2] refactor(bloglist): modularize app structure

---
+++ part4/bloglist/utils/middleware.js
@@ -0,0 +1,29 @@
+const morgan = require('morgan');
+const logger = require('./logger');
+
+morgan.token('data', (request) => {
+  return JSON.stringify(request.body);
+});
+
+const requestLogger = morgan(
+  ':method :url :status :res[content-length] - :response-time ms :data',
+);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).json({ error: 'Unknown endpoint' });
+};
+
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).json({ error: 'Malformed ID' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  return response.status(500).json({ error: 'Internal server error' });
+};
+
+const middleware = { requestLogger, unknownEndpoint, errorHandler };
+module.exports = middleware;
