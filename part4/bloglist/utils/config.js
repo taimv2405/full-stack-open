eef10834f02e399ce057cc0e73bf05760273bcba
taimv2405 <taimv2405@gmail.com>
2026-05-25 10:15:35 +0000

    [4.15] feat(bloglist): implement POST and GET /api/users endpoint

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -5,6 +5,7 @@ const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
+const BCRYPT_SALT_ROUNDS = Number(process.env.BCRYPT_SALT_ROUNDS) || 10;
 
-const config = { PORT, MONGODB_URI };
+const config = { PORT, MONGODB_URI, BCRYPT_SALT_ROUNDS };
 module.exports = config;
