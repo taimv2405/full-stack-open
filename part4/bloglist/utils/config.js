0b20599e0d1d9623bd4e268702045f1bd3f882ff
taimv2405 <taimv2405@gmail.com>
2026-05-25 15:25:42 +0000

    [4.18] feat(bloglist): implement token-based authentication

--- part4/bloglist/utils/config.js
+++ part4/bloglist/utils/config.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+
 const MONGODB_URI =
   process.env.NODE_ENV === 'test'
     ? process.env.TEST_MONGODB_URI
     : process.env.MONGODB_URI;
+
 const BCRYPT_SALT_ROUNDS = Number(process.env.BCRYPT_SALT_ROUNDS) || 10;
 
-const config = { PORT, MONGODB_URI, BCRYPT_SALT_ROUNDS };
+const SECRET = process.env.SECRET;
+
+const config = { PORT, MONGODB_URI, BCRYPT_SALT_ROUNDS, SECRET };
 module.exports = config;
