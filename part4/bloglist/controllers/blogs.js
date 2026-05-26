f514c1fb0bf30f8bb99fe72e1153134493a88838
taimv2405 <taimv2405@gmail.com>
2026-05-26 07:47:24 +0000

    [4.22] refactor(bloglist): add userExtractor middleware

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,25 +1,17 @@
 const blogsRouter = require('express').Router();
-const jwt = require('jsonwebtoken');
 
 const Blog = require('../models/blog');
-const User = require('../models/user');
-const config = require('../utils/config');
+const middleware = require('../utils/middleware');
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 });
   response.json(blogs);
 });
 
-blogsRouter.post('/', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, config.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'invalid token' });
-  }
-
-  const user = await User.findById(decodedToken.id);
-  if (!user) return response.status(400).json({ error: 'user not found' });
-
-  const blog = new Blog({ ...request.body, user: user._id });
+blogsRouter.post('/', middleware.userExtractor, async (request, response) => {
+  const user = request.user;
+  const { title, author, url, likes } = request.body;
+  const blog = new Blog({ title, author, url, likes, user: user.id });
   const savedBlog = await blog.save();
 
   user.blogs = [...user.blogs, savedBlog._id];
@@ -27,26 +19,25 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(savedBlog);
 });
 
-blogsRouter.delete('/:id', async (request, response) => {
-  const decodedToken = jwt.verify(request.token, config.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'invalid token' });
-  }
+blogsRouter.delete(
+  '/:id',
+  middleware.userExtractor,
+  async (request, response) => {
+    const blog = await Blog.findById(request.params.id);
+    if (!blog) {
+      return response
+        .status(404)
+        .json({ error: 'blog not found or already deleted' });
+    }
 
-  const blog = await Blog.findById(request.params.id);
-  if (!blog) {
-    return response
-      .status(404)
-      .json({ error: 'blog not found or already deleted' });
-  }
+    if (!blog.user || blog.user.toString() !== request.user.id) {
+      return response.status(403).json({ error: 'no permission' });
+    }
 
-  if (!blog.user || blog.user.toString() !== decodedToken.id) {
-    return response.status(403).json({ error: 'no permission' });
-  }
-
-  await blog.deleteOne();
-  response.status(204).end();
-});
+    await blog.deleteOne();
+    response.status(204).end();
+  },
+);
 
 blogsRouter.put('/:id', async (request, response) => {
   const blog = await Blog.findById(request.params.id);
