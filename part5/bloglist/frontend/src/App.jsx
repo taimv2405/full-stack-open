4f38cb2a7a6be5c372ed4d436ce33ccf05f6b2bf
taimv2405 <taimv2405@gmail.com>
2026-05-27 16:00:05 +0000

    [5.3] feat(bloglist): add create blog form and service

--- part5/bloglist/frontend/src/App.jsx
+++ part5/bloglist/frontend/src/App.jsx
@@ -7,6 +7,9 @@ const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -16,7 +19,9 @@ const App = () => {
   useEffect(() => {
     const loggedUserJSON = window.localStorage.getItem('loggedBloglistUser');
     if (loggedUserJSON) {
-      setUser(JSON.parse(loggedUserJSON));
+      const user = JSON.parse(loggedUserJSON);
+      blogService.setToken(user.token);
+      setUser(user);
     }
   }, []);
 
@@ -24,7 +29,9 @@ const App = () => {
     event.preventDefault();
     try {
       const user = await loginService.login({ username, password });
+
       window.localStorage.setItem('loggedBloglistUser', JSON.stringify(user));
+      blogService.setToken(user.token);
       setUser(user);
       setUsername('');
       setPassword('');
@@ -36,6 +43,19 @@ const App = () => {
   const handleLogout = () => {
     window.localStorage.removeItem('loggedBloglistUser');
     setUser(null);
+  };
+
+  const handleCreateBlog = async (event) => {
+    event.preventDefault();
+    try {
+      const createdBlog = await blogService.create({ title, author, url });
+      setBlogs([...blogs, createdBlog]);
+      setTitle('');
+      setAuthor('');
+      setUrl('');
+    } catch (error) {
+      console.error(error.response?.data?.error);
+    }
   };
 
   return (
@@ -73,10 +93,46 @@ const App = () => {
         <div>
           <h2>blogs</h2>
           <p>{user.name} logged in</p>
+          <button onClick={handleLogout}>log out</button>
+
+          <h2>create new</h2>
+          <form onSubmit={handleCreateBlog}>
+            <div>
+              <label>
+                title
+                <input
+                  type="text"
+                  value={title}
+                  onChange={({ target }) => setTitle(target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                author
+                <input
+                  type="text"
+                  value={author}
+                  onChange={({ target }) => setAuthor(target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label>
+                url
+                <input
+                  type="text"
+                  value={url}
+                  onChange={({ target }) => setUrl(target.value)}
+                />
+              </label>
+            </div>
+            <button type="submit">create</button>
+          </form>
+
           {blogs.map((blog) => (
             <Blog key={blog.id} blog={blog} />
           ))}
-          <button onClick={handleLogout}>log out</button>
         </div>
       )}
     </>
--- part5/bloglist/frontend/src/services/blogs.js
+++ part5/bloglist/frontend/src/services/blogs.js
@@ -1,9 +1,24 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from 'axios';
+const baseUrl = '/api/blogs';
+
+let token = null;
+
+const setToken = (newToken) => {
+  token = newToken;
+};
+
+const getAuthHeader = () => (token ? { Authorization: `Bearer ${token}` } : {});
 
 const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+  const request = axios.get(baseUrl);
+  return request.then((response) => response.data);
+};
 
-export default { getAll }
+const create = (newBlog) => {
+  const request = axios.post(baseUrl, newBlog, {
+    headers: getAuthHeader(),
+  });
+  return request.then((response) => response.data);
+};
+
+export default { getAll, create, setToken };
