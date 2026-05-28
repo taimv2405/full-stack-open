f6cc4a0a139a1d45aa9f1cea7bc266f0bab3776e
taimv2405 <taimv2405@gmail.com>
2026-05-28 05:30:06 +0000

    [5.7] feat(bloglist): toggle blog detail view

--- part5/bloglist/frontend/src/components/Blog.jsx
+++ part5/bloglist/frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [detail, setDetail] = useState(false);
+
+  const blogStyle = {
+    border: '1px solid black',
+    padding: '8px 16px',
+    marginBottom: 5,
+  };
+
+  const toggleShowDetail = () => {
+    setDetail(!detail);
+  };
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleShowDetail}>{detail ? 'hide' : 'view'}</button>
+      </div>
+      {detail && (
+        <div>
+          <div>{blog.url}</div>
+          <div>
+            likes {blog.likes} <button>like</button>
+          </div>
+          <div>{blog.user?.name}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
