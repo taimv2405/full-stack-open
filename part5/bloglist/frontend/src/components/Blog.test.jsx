ea4b0dcbf82b2f5c92203600ce0d6826b42da17c
taimv2405 <taimv2405@gmail.com>
2026-05-28 11:50:14 +0000

    [5.14] test(bloglist): Blog shows url and likes after view click

--- part5/bloglist/frontend/src/components/Blog.test.jsx
+++ part5/bloglist/frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Blog from './Blog';
 
 test('renders title and author but not url and likes by default', () => {
@@ -17,4 +18,21 @@ test('renders title and author but not url and likes by default', () => {
     screen.queryByText(blog.url, { exact: false }),
   ).not.toBeInTheDocument();
   expect(screen.queryByText(/likes:?\s*7/i)).not.toBeInTheDocument();
+});
+
+test('show URL and number of likes when view details button has been clicked', async () => {
+  const user = userEvent.setup();
+  const blog = {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  };
+
+  render(<Blog blog={blog} />);
+  const viewBtn = screen.getByText(/view/i);
+  await user.click(viewBtn);
+
+  expect(screen.getByText(blog.url, { exact: false })).toBeInTheDocument();
+  expect(screen.getByText(/likes:?\s*7/i)).toBeInTheDocument();
 });
