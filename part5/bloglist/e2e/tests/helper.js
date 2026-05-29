61de95a2fb68eb1d8babfe7d6f34c4edc8316221
taimv2405 <taimv2405@gmail.com>
2026-05-29 11:01:39 +0000

    refactor(bloglist): clean and DRY e2e test suite

--- part5/bloglist/e2e/tests/helper.js
+++ part5/bloglist/e2e/tests/helper.js
@@ -4,7 +4,7 @@ const loginWith = async (page, username, password) => {
   await page.getByRole('button', { name: 'login' }).click();
 };
 
-const createBlog = async (page, title, author, url) => {
+const createBlog = async (page, { title, author, url }) => {
   await page.getByRole('button', { name: 'create new blog' }).click();
   await page.getByLabel('title').fill(title);
   await page.getByLabel('author').fill(author);
@@ -13,4 +13,16 @@ const createBlog = async (page, title, author, url) => {
   await page.getByText(`${title} ${author}`).waitFor();
 };
 
-module.exports = { loginWith, createBlog };
+const getBlogRow = (page, { title, author }) =>
+  page.getByText(`${title} ${author}`).locator('..');
+
+const likeNTimes = async (page, blogRow, n) => {
+  await blogRow.getByRole('button', { name: 'view' }).click();
+  for (let i = 1; i <= n; i++) {
+    await page.getByRole('button', { name: 'like' }).click();
+    await page.getByText(`likes ${i}`).waitFor();
+  }
+  await blogRow.getByRole('button', { name: 'hide' }).click();
+};
+
+module.exports = { loginWith, createBlog, getBlogRow, likeNTimes };
