ea2c983c3c5af758c399c2ba8e798af37308f228
taimv2405 <taimv2405@gmail.com>
2026-05-29 08:53:42 +0000

    [5.18] test(bloglist): login succeeds and fails with wrong credentials

--- part5/bloglist/e2e/tests/blog_app.spec.js
+++ part5/bloglist/e2e/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
+const { loginWith } = require('./helper');
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -17,5 +18,22 @@ describe('Blog app', () => {
     await expect(page.getByLabel('username')).toBeVisible();
     await expect(page.getByLabel('password')).toBeVisible();
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible();
+  });
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      await loginWith(page, 'anna', '123456');
+      await expect(page.getByText('Anna logged in')).toBeVisible();
+    });
+
+    test('fails with wrong credentials', async ({ page }) => {
+      await loginWith(page, 'anna', 'wrong');
+
+      const errorDiv = page.locator('.error');
+      await expect(errorDiv).toContainText('invalid username or password');
+      await expect(errorDiv).toHaveCSS('border-style', 'solid');
+      await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)');
+      await expect(page.getByText('Anna logged in')).not.toBeVisible();
+    });
   });
 });
