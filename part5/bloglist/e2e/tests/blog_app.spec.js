0a17afd88d3cf80c6dea798700dc4b814714c406
taimv2405 <taimv2405@gmail.com>
2026-05-29 08:53:21 +0000

    [5.18] test(bloglist): reset db and create user in beforeEach

--- part5/bloglist/e2e/tests/blog_app.spec.js
+++ part5/bloglist/e2e/tests/blog_app.spec.js
@@ -1,7 +1,15 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset');
+    await request.post('/api/users', {
+      data: {
+        username: 'anna',
+        password: '123456',
+        name: 'Anna',
+      },
+    });
     await page.goto('/');
   });
 
