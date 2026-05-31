0686ce5c63f3bd42bfcf6bbdbd41c319c95a1bf8
taimv2405 <taimv2405@gmail.com>
2026-05-31 10:18:07 +0000

    [5.28] test(bloglist): rewrite e2e tests for routed app

--- part5/bloglist/e2e/tests/blog_app.spec.js
+++ part5/bloglist/e2e/tests/blog_app.spec.js
@@ -1,5 +1,11 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test');
-const { loginWith, createBlog, getBlogRow, likeNTimes } = require('./helper');
+const {
+  loginWith,
+  createBlog,
+  goToBlog,
+  likeNTimes,
+  goToHome,
+} = require('./helper');
 
 const ANNA = { username: 'anna', password: '123456', name: 'Anna' };
 const BOB = { username: 'bob', password: '123456', name: 'Bob' };
@@ -28,6 +34,7 @@ describe('Blog app', () => {
   });
 
   test('Login form is shown', async ({ page }) => {
+    await page.getByRole('link', { name: 'login' }).click();
     await expect(page.getByLabel('username')).toBeVisible();
     await expect(page.getByLabel('password')).toBeVisible();
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible();
@@ -37,6 +44,8 @@ describe('Blog app', () => {
     test('succeeds with correct credentials', async ({ page }) => {
       await loginWith(page, ANNA.username, ANNA.password);
       await expect(page.getByText('Anna logged in')).toBeVisible();
+      await expect(page.getByRole('link', { name: 'login' })).not.toBeVisible();
+      await expect(page.getByRole('button', { name: 'log out' })).toBeVisible();
     });
 
     test('fails with wrong credentials', async ({ page }) => {
@@ -47,6 +56,10 @@ describe('Blog app', () => {
       await expect(errorDiv).toHaveCSS('border-style', 'solid');
       await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)');
       await expect(page.getByText('Anna logged in')).not.toBeVisible();
+      await expect(page.getByRole('link', { name: 'login' })).toBeVisible();
+      await expect(
+        page.getByRole('button', { name: 'log out' }),
+      ).not.toBeVisible();
     });
   });
 
@@ -59,22 +72,31 @@ describe('Blog app', () => {
       await createBlog(page, FIRST_CLASS);
 
       await expect(
-        page.getByText(`a new blog ${FIRST_CLASS.title} by ${FIRST_CLASS.author}`),
+        page.getByText(
+          `a new blog ${FIRST_CLASS.title} by ${FIRST_CLASS.author}`,
+        ),
       ).toBeVisible();
       await expect(
-        page.getByText(`${FIRST_CLASS.title} ${FIRST_CLASS.author}`),
+        page.getByRole('link', {
+          name: `${FIRST_CLASS.title} by ${FIRST_CLASS.author}`,
+        }),
       ).toBeVisible();
     });
 
-    test('only creator can see blog delete button', async ({ page, request }) => {
+    test('only creator can see blog delete button', async ({
+      page,
+      request,
+    }) => {
       await request.post('/api/users', { data: BOB });
       await createBlog(page, GOTO);
       await page.getByRole('button', { name: 'log out' }).click();
 
       await loginWith(page, BOB.username, BOB.password);
-      await getBlogRow(page, GOTO).getByRole('button', { name: 'view' }).click();
+      await goToBlog(page, GOTO);
 
-      await expect(page.getByRole('button', { name: 'remove' })).not.toBeVisible();
+      await expect(
+        page.getByRole('button', { name: 'remove' }),
+      ).not.toBeVisible();
     });
 
     describe('and several blogs exist', () => {
@@ -85,34 +107,47 @@ describe('Blog app', () => {
       });
 
       test('one of those can be liked', async ({ page }) => {
-        await getBlogRow(page, GOTO).getByRole('button', { name: 'view' }).click();
+        await goToBlog(page, FIRST_CLASS);
         await page.getByRole('button', { name: 'like' }).click();
         await expect(page.getByText('likes 1')).toBeVisible();
       });
 
       test('blog creator can delete the blog', async ({ page }) => {
-        await getBlogRow(page, GOTO).getByRole('button', { name: 'view' }).click();
+        await goToBlog(page, GOTO);
         page.on('dialog', (dialog) => dialog.accept());
         await page.getByRole('button', { name: 'remove' }).click();
-
+        await page.getByRole('heading', { name: 'blogs', level: 1 }).waitFor();
         await expect(
-          page.getByText(`${GOTO.title} ${GOTO.author}`),
+          page.getByText(`${GOTO.title} by ${GOTO.author}`),
         ).not.toBeVisible();
       });
 
       test('blogs are arranged in likes desc order', async ({ page }) => {
-        await likeNTimes(page, getBlogRow(page, FIRST_CLASS), 1);
-        await likeNTimes(page, getBlogRow(page, GOTO), 2);
-        await likeNTimes(page, getBlogRow(page, CANONICAL), 3);
+        await goToBlog(page, FIRST_CLASS);
+        await likeNTimes(page, 1);
+        await goToHome(page);
 
-        const rows = await page
-          .getByRole('button', { name: 'view' })
-          .locator('..')
-          .all();
+        await goToBlog(page, GOTO);
+        await likeNTimes(page, 2);
+        await goToHome(page);
 
-        await expect(rows[0].getByText(`${CANONICAL.title} ${CANONICAL.author}`)).toBeVisible();
-        await expect(rows[1].getByText(`${GOTO.title} ${GOTO.author}`)).toBeVisible();
-        await expect(rows[2].getByText(`${FIRST_CLASS.title} ${FIRST_CLASS.author}`)).toBeVisible();
+        await goToBlog(page, CANONICAL);
+        await likeNTimes(page, 3);
+        await goToHome(page);
+
+        const rows = await page.getByRole('listitem');
+
+        await expect(
+          rows.nth(0).getByText(`${CANONICAL.title} by ${CANONICAL.author}`),
+        ).toBeVisible();
+        await expect(
+          rows.nth(1).getByText(`${GOTO.title} by ${GOTO.author}`),
+        ).toBeVisible();
+        await expect(
+          rows
+            .nth(2)
+            .getByText(`${FIRST_CLASS.title} by ${FIRST_CLASS.author}`),
+        ).toBeVisible();
       });
     });
   });
