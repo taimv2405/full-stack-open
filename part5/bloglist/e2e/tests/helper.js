49c358f0ffa0536867d06f694a1d2fcadfa7df82
taimv2405 <taimv2405@gmail.com>
2026-05-31 10:18:07 +0000

    [5.28] test(bloglist): rewrite e2e tests for routed app

--- part5/bloglist/e2e/tests/helper.js
+++ part5/bloglist/e2e/tests/helper.js
@@ -1,28 +1,34 @@
 const loginWith = async (page, username, password) => {
+  await page.getByRole('link', { name: 'login' }).click();
   await page.getByLabel('username').fill(username);
   await page.getByLabel('password').fill(password);
   await page.getByRole('button', { name: 'login' }).click();
 };
 
 const createBlog = async (page, { title, author, url }) => {
-  await page.getByRole('button', { name: 'create new blog' }).click();
+  await page.getByRole('link', { name: 'new blog' }).click();
   await page.getByLabel('title').fill(title);
   await page.getByLabel('author').fill(author);
   await page.getByLabel('url').fill(url);
   await page.getByRole('button', { name: 'create' }).click();
-  await page.getByText(`${title} ${author}`).waitFor();
+  await page
+    .getByRole('link', {
+      name: `${title} by ${author}`,
+    })
+    .waitFor();
 };
 
-const getBlogRow = (page, { title, author }) =>
-  page.getByText(`${title} ${author}`).locator('..');
+const goToBlog = async (page, { title, author }) =>
+  await page.getByRole('link', { name: `${title} by ${author}` }).click();
 
-const likeNTimes = async (page, blogRow, n) => {
-  await blogRow.getByRole('button', { name: 'view' }).click();
+const likeNTimes = async (page, n) => {
   for (let i = 1; i <= n; i++) {
     await page.getByRole('button', { name: 'like' }).click();
     await page.getByText(`likes ${i}`).waitFor();
   }
-  await blogRow.getByRole('button', { name: 'hide' }).click();
 };
 
-module.exports = { loginWith, createBlog, getBlogRow, likeNTimes };
+const goToHome = async (page) =>
+  await page.getByRole('link', { name: 'blogs' }).click();
+
+module.exports = { loginWith, createBlog, goToBlog, likeNTimes, goToHome };
