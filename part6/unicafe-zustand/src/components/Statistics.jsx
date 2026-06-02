94b031dd494ca362efa224866592ee4e2e32f717
taimv2405 <taimv2405@gmail.com>
2026-06-02 08:24:55 +0000

    [6.1] feat(unicafe): implement feedback store with Zustand

--- part6/unicafe-zustand/src/components/Statistics.jsx
+++ part6/unicafe-zustand/src/components/Statistics.jsx
@@ -1,26 +1,50 @@
+import { useBad, useGood, useNeutral } from '../store';
+
 const Statistics = () => {
-  const good = 0
-  const neutral = 0
-  const bad = 0
-  const all = 0
-  const average = 0
-  const positive = 0
-  
+  const good = useGood();
+  const neutral = useNeutral();
+  const bad = useBad();
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = (good / all) * 100;
+
   return (
     <div>
       <h2>statistics</h2>
-      <table>
-        <tbody>
-          <tr><td>good</td><td>{good}</td></tr>
-          <tr><td>neutral</td><td>{neutral}</td></tr>
-          <tr><td>bad</td><td>{bad}</td></tr>
-          <tr><td>all</td><td>{all}</td></tr>
-          <tr><td>average</td><td>{average}</td></tr>
-          <tr><td>positive</td><td>{positive}</td></tr>
-        </tbody>
-      </table>
+      {all > 0 ? (
+        <table>
+          <tbody>
+            <tr>
+              <td>good</td>
+              <td>{good}</td>
+            </tr>
+            <tr>
+              <td>neutral</td>
+              <td>{neutral}</td>
+            </tr>
+            <tr>
+              <td>bad</td>
+              <td>{bad}</td>
+            </tr>
+            <tr>
+              <td>all</td>
+              <td>{all}</td>
+            </tr>
+            <tr>
+              <td>average</td>
+              <td>{average}</td>
+            </tr>
+            <tr>
+              <td>positive</td>
+              <td>{positive} %</td>
+            </tr>
+          </tbody>
+        </table>
+      ) : (
+        <p>No feedback given</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Statistics
+export default Statistics;
