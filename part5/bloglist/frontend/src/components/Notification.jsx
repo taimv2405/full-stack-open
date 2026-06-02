bd55350e104344cc459866182aff4522148f1545
taimv2405 <taimv2405@gmail.com>
2026-06-02 06:25:10 +0000

    [5.30] feat(bloglist): style navbar and notifications with MUI

--- part5/bloglist/frontend/src/components/Notification.jsx
+++ part5/bloglist/frontend/src/components/Notification.jsx
@@ -1,8 +1,10 @@
+import { Alert } from '@mui/material';
+
 const Notification = ({ notification }) => {
   if (!notification) return null;
 
   const { message, type } = notification;
-  return <p className={`notification ${type}`}>{message}</p>;
+  return <Alert severity={type}>{message}</Alert>;
 };
 
 export default Notification;
