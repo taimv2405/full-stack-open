38abc4700b6d0e34f6aa6eeb6d032beab89e0e21
taimv2405 <taimv2405@gmail.com>
2026-06-03 10:44:41 +0000

    [6.11] feat(anecdotes): delete zero-vote anecdotes

--- part6/zustand-anecdotes/src/components/AnecdoteList.jsx
+++ part6/zustand-anecdotes/src/components/AnecdoteList.jsx
@@ -6,7 +6,7 @@ import {
 
 const AnecdoteList = () => {
   const anecdotes = useAnecdotes();
-  const { vote } = useAnecdoteActions();
+  const { vote, remove } = useAnecdoteActions();
   const { setMessage } = useNotificationActions();
   const sortedAnecdotes = anecdotes.toSorted((a, b) => b.votes - a.votes);
 
@@ -14,6 +14,18 @@ const AnecdoteList = () => {
     vote(anecdote.id);
     setMessage(`You voted '${anecdote.content}'`);
     setTimeout(() => setMessage(null), 5000);
+  };
+
+  const handleRemove = async (anecdote) => {
+    if (confirm(`Are you sure to remove ${anecdote.content}`)) {
+      try {
+        await remove(anecdote.id);
+        setMessage(`You removed '${anecdote.content}'`);
+      } catch (error) {
+        setMessage(error.message);
+      }
+      setTimeout(() => setMessage(null), 5000);
+    }
   };
 
   return (
@@ -24,6 +36,9 @@ const AnecdoteList = () => {
           <div>
             has {anecdote.votes} votes
             <button onClick={() => handleVote(anecdote)}>vote</button>
+            {anecdote.votes === 0 && (
+              <button onClick={() => handleRemove(anecdote)}>remove</button>
+            )}
           </div>
         </div>
       ))}
