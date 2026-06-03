6249a8eb843f1e1e130c20b9ddc5e8d25af0e954
taimv2405 <taimv2405@gmail.com>
2026-06-03 04:56:51 +0000

    [6.6] feat(anecdotes): filter anecdotes by content string

--- part6/zustand-anecdotes/src/store.js
+++ part6/zustand-anecdotes/src/store.js
@@ -19,6 +19,7 @@ const asObject = (anecdote) => ({
 
 const useAnecdoteStore = create((set) => ({
   anecdotes: anecdotesAtStart.map(asObject),
+  filter: '',
   actions: {
     vote: (id) =>
       set((state) => ({
@@ -30,9 +31,16 @@ const useAnecdoteStore = create((set) => ({
       })),
     add: (anecdote) =>
       set((state) => ({ anecdotes: [...state.anecdotes, asObject(anecdote)] })),
+    setFilter: (newFilter) => set(() => ({ filter: newFilter })),
   },
 }));
 
-export const useAnecdotes = () => useAnecdoteStore((state) => state.anecdotes);
+export const useAnecdotes = () => {
+  const anecdotes = useAnecdoteStore((state) => state.anecdotes);
+  const filter = useAnecdoteStore((state) => state.filter);
+  return anecdotes.filter((anecdote) =>
+    anecdote.content.toLowerCase().includes(filter.toLowerCase()),
+  );
+};
 export const useAnecdoteActions = () =>
   useAnecdoteStore((state) => state.actions);
