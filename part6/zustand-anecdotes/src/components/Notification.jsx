964447582201d9ae8a55a70854cdf390fb5e58eb
taimv2405 <taimv2405@gmail.com>
2026-06-03 10:26:33 +0000

    [6.10] feat(anecdotes): show notification on vote and create

--- part6/zustand-anecdotes/src/components/Notification.jsx
+++ part6/zustand-anecdotes/src/components/Notification.jsx
@@ -1,12 +1,17 @@
+import { useMessage } from '../store';
+
 const Notification = () => {
+  const message = useMessage();
+  if (!message) return null;
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
-    marginBottom: 10
-  }
+    marginBottom: 10,
+  };
 
-  return <div style={style}>render here notification...</div>
-}
+  return <div style={style}>{message}</div>;
+};
 
-export default Notification
+export default Notification;
