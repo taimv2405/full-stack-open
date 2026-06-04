3241e34bea101145d37fb67844cd827de85c4637
taimv2405 <taimv2405@gmail.com>
2026-06-04 06:06:45 +0000

    feat(anecdotes): add zustand devtools middleware

--- part6/zustand-anecdotes/src/store.js
+++ part6/zustand-anecdotes/src/store.js
@@ -1,56 +1,59 @@
 import { create } from 'zustand';
 import anecdoteService from './services/anecdoteService';
+import { devtools } from 'zustand/middleware';
 
-const useAnecdoteStore = create((set, get) => ({
-  anecdotes: [],
-  filter: '',
-  actions: {
-    vote: async (id) => {
-      const targetAnecdote = get().anecdotes.find(
-        (anecdote) => anecdote.id === id,
-      );
+const useAnecdoteStore = create(
+  devtools((set, get) => ({
+    anecdotes: [],
+    filter: '',
+    actions: {
+      vote: async (id) => {
+        const targetAnecdote = get().anecdotes.find(
+          (anecdote) => anecdote.id === id,
+        );
 
-      await anecdoteService.update(id, {
-        ...targetAnecdote,
-        votes: targetAnecdote.votes + 1,
-      });
+        await anecdoteService.update(id, {
+          ...targetAnecdote,
+          votes: targetAnecdote.votes + 1,
+        });
 
-      set((state) => ({
-        anecdotes: state.anecdotes.map((anecdote) =>
-          anecdote.id === id
-            ? { ...anecdote, votes: anecdote.votes + 1 }
-            : anecdote,
-        ),
-      }));
+        set((state) => ({
+          anecdotes: state.anecdotes.map((anecdote) =>
+            anecdote.id === id
+              ? { ...anecdote, votes: anecdote.votes + 1 }
+              : anecdote,
+          ),
+        }));
+      },
+      add: async (content) => {
+        const anecdote = await anecdoteService.create({ content, votes: 0 });
+        set((state) => ({ anecdotes: [...state.anecdotes, anecdote] }));
+      },
+      setFilter: (newFilter) => set(() => ({ filter: newFilter })),
+      initialize: async () => {
+        try {
+          const returnedAnecdotes = await anecdoteService.getAll();
+          set(() => ({ anecdotes: returnedAnecdotes }));
+        } catch (error) {
+          console.error(error.message);
+        }
+      },
+      remove: async (id) => {
+        const targetAnecdote = get().anecdotes.find(
+          (anecdote) => anecdote.id === id,
+        );
+        if (targetAnecdote.votes > 0)
+          throw new Error('Only zero votes anecdotes can be removed');
+
+        await anecdoteService.remove(id);
+
+        set((state) => ({
+          anecdotes: state.anecdotes.filter((anecdote) => anecdote.id !== id),
+        }));
+      },
     },
-    add: async (content) => {
-      const anecdote = await anecdoteService.create({ content, votes: 0 });
-      set((state) => ({ anecdotes: [...state.anecdotes, anecdote] }));
-    },
-    setFilter: (newFilter) => set(() => ({ filter: newFilter })),
-    initialize: async () => {
-      try {
-        const returnedAnecdotes = await anecdoteService.getAll();
-        set(() => ({ anecdotes: returnedAnecdotes }));
-      } catch (error) {
-        console.error(error.message);
-      }
-    },
-    remove: async (id) => {
-      const targetAnecdote = get().anecdotes.find(
-        (anecdote) => anecdote.id === id,
-      );
-      if (targetAnecdote.votes > 0)
-        throw new Error('Only zero votes anecdotes can be removed');
-
-      await anecdoteService.remove(id);
-
-      set((state) => ({
-        anecdotes: state.anecdotes.filter((anecdote) => anecdote.id !== id),
-      }));
-    },
-  },
-}));
+  })),
+);
 
 export const useAnecdotes = () => {
   const anecdotes = useAnecdoteStore((state) => state.anecdotes);
