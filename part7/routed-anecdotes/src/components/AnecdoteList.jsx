b17c4a575bbfc6cde4b76ee8ea8e56ed122128d5
taimv2405 <taimv2405@gmail.com>
2026-06-06 09:56:00 +0000

    [7.6] feat(anecdotes): add delete anecdote

--- part7/routed-anecdotes/src/components/AnecdoteList.jsx
+++ part7/routed-anecdotes/src/components/AnecdoteList.jsx
@@ -1,10 +1,17 @@
-const AnecdoteList = ({ anecdotes }) => (
-  <div>
-    <h2>Anecdotes</h2>
-    <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id}>{anecdote.content}</li>)}
-    </ul>
-  </div>
-)
+const AnecdoteList = ({ anecdotes, removeAnecdote }) => {
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      <ul>
+        {anecdotes.map((anecdote) => (
+          <li key={anecdote.id}>
+            {anecdote.content}{' '}
+            <button onClick={() => removeAnecdote(anecdote.id)}>delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default AnecdoteList
+export default AnecdoteList;
--- part7/routed-anecdotes/src/services/anecdotes.js
+++ part7/routed-anecdotes/src/services/anecdotes.js
@@ -1,27 +1,37 @@
-const baseUrl = 'http://localhost:3001/anecdotes'
+const baseUrl = 'http://localhost:3001/anecdotes';
 
 const getAll = async () => {
-  const response = await fetch(baseUrl)
+  const response = await fetch(baseUrl);
 
   if (!response.ok) {
-    throw new Error('Failed to fetch notes')
+    throw new Error('Failed to fetch notes');
   }
 
-  return await response.json()
-}
+  return await response.json();
+};
 
 const createNew = async (object) => {
   const response = await fetch(baseUrl, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(object),
-  })
-  
-  if (!response.ok) {
-    throw new Error('Failed to create note')
-  }
-  
-  return await response.json()
-}
+  });
 
-export default { getAll, createNew }
+  if (!response.ok) {
+    throw new Error('Failed to create note');
+  }
+
+  return await response.json();
+};
+
+const remove = async (id) => {
+  const response = await fetch(`${baseUrl}/${id}`, { method: 'DELETE' });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch notes');
+  }
+
+  return await response.json();
+};
+
+export default { getAll, createNew, remove };
