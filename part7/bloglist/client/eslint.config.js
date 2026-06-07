4f7de9e93d30f8014baf72727dd8a5fef7c1673d
taimv2405 <taimv2405@gmail.com>
2026-06-07 08:51:26 +0000

    [7.10] chore(bloglist): add prettier with eslint integration

--- part7/bloglist/client/eslint.config.js
+++ part7/bloglist/client/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import prettier from 'eslint-config-prettier';
 
 export default [
   { ignores: ['dist'] },
@@ -47,4 +48,5 @@ export default [
       },
     },
   },
+  prettier,
 ];
